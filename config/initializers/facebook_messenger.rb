1f159a261c70cf15fb68d0677d9436df0eb233e4
Chandresh Sankhala <chandresh@auza.in>
2026-08-13 14:46:20 +0530

    fix(facebook): perform exact verify token comparison in ChatwootFbProvider

--- config/initializers/facebook_messenger.rb
+++ config/initializers/facebook_messenger.rb
@@ -2,8 +2,9 @@
 class ChatwootFbProvider < Facebook::Messenger::Configuration::Providers::Base
   CHANNEL_APP_SECRET_KEYS = %w[app_secret app_secret_key client_secret api_secret].freeze
 
-  def valid_verify_token?(_verify_token)
-    GlobalConfigService.load('FB_VERIFY_TOKEN', '')
+  def valid_verify_token?(verify_token)
+    expected_token = GlobalConfigService.load('FB_VERIFY_TOKEN', '')
+    expected_token.present? && verify_token == expected_token
   end
 
   def app_secret_for(page_id)

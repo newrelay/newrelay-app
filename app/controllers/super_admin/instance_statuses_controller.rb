86a6f2ec4931cfd979d59ee3753af5400eec34f7
Chandresh Sankhala <chandresh@auza.in>
2026-09-24 11:57:57 +0530

    fix(admin): replace leftover Chatwoot labels with NewRelay in Super Admin
    Administrate was using the Rails app class name in the tab title, and Settings/feature copy still said Chatwoot. Super Admin now shows NewRelay branding and Relay AI activity logs.

--- app/controllers/super_admin/instance_statuses_controller.rb
+++ app/controllers/super_admin/instance_statuses_controller.rb
@@ -10,7 +10,7 @@ class SuperAdmin::InstanceStatusesController < SuperAdmin::ApplicationController
   end
 
   def chatwoot_edition
-    @metrics['newrelay edition'] =
+    @metrics['NewRelay edition'] =
       if ChatwootApp.enterprise?
         'Enterprise'
       elsif ChatwootApp.custom?
@@ -25,7 +25,7 @@ class SuperAdmin::InstanceStatusesController < SuperAdmin::ApplicationController
   end
 
   def chatwoot_version
-    @metrics['newrelay version'] = Chatwoot.config[:version]
+    @metrics['NewRelay version'] = Chatwoot.config[:version]
   end
 
 
--- app/helpers/super_admin/ui_helper.rb
+++ app/helpers/super_admin/ui_helper.rb
@@ -1,4 +1,9 @@
 module SuperAdmin::UiHelper
+  # Administrate's helper uses Rails.application.module_parent_name ("Chatwoot").
+  def application_title
+    'NewRelay'
+  end
+
   def super_admin_feature_icon(feature_key)
     inner = SuperAdmin::AccountFeaturesHelper.icon_inner_html(feature_key)
     %(<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">#{inner}</svg>).html_safe
